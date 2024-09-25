349fae93da9b6cdb4d97b0cefbe7610d29ddefe9
NIMMY04 <156294603+NIMMY04@users.noreply.github.com>
2024-09-25 15:01:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll('.circles');
+const nextbutton = document.querySelectorById('next');
+const prevbutton = document.querySelectorById('prev');
+let currentActive = 0;
+
+nextbutton.addEventListner('click',() =>{
+    currentActive++;
+    updateProgress();
+});
+
+prevbutton.addEventListner('click',() =>{
+    currentActive--;
+    updateProgress();
+})
+
+function updateProgress(){
+    circles.forEach((circle,index)=>{
+        if(index < currentActive + 1){
+            circle.classList.add('active');
+    }
+else{
+    circle.classList.remove('active');
+}
+});
+prevButton.disabled = currentActive === 0;
+nextbutton.disabled = curtentActive === circles.length -1;
+}
